c73117ebc5810a795c9239f60f28db856b22216a
mesfint <mesfin_meng@yahoo.com>
2024-03-22 18:49:34 +0200

    sign-up page completed

--- realestate/api/index.js
+++ realestate/api/index.js
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user-route.js";
 import authRouter from "./routes/auth.route.js";
 import middleware from "../middleware/middleware.js";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use(middleware.errorHandler);
